c7049f14af1ba0c09e7457bc5dace92911f7fa21
adigitX <adityakammati.workspace@gmail.com>
2026-02-19 23:12:18 +0530

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -14,8 +15,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["'Saprona'", "system-ui", "sans-serif"],
-        mono: ["IBM Plex Mono", "monospace"],
+        sans: ["'Saprona'"],
+        mono: ["'Saprona'"],
       },
       colors: {
         border: "hsl(var(--border))",
@@ -93,5 +94,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
